8af5e45f4394bd128f1e9b8cbb973715641fd07b
mdsabbir112 <anikmullah112@gmail.com>
2025-12-07 23:09:59 +0600

    added Questation and Footer section

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+// Accordion Functionality
+
+const FAQ = () => {
+    document.getElementById("questation").addEventListener("click", function () {.0 
+        document.getElementById("faqSection").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+}
+FAQ();
